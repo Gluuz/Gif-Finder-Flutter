474ac12a48675e170e4fdba13b0ebbfce6422031
Bruno Gluszczuk <brunogluszczukk1@gmail.com>
2021-10-29 12:00:32 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/gif_searcher/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gif_searcher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
